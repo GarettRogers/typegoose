7d827767aeb30f9e99000a4f53a755122e5a600b
Garett <garettr@iqmetrix.com>
2020-10-08 20:30:44 -0700

    adding peer dependency

--- src/typegoose.ts
+++ src/typegoose.ts
@@ -1,8 +1,6 @@
 /* imports */
 import 'reflect-metadata';
 
-import { getName } from './internal/utils';
-
 import { parseENV, setGlobalOptions } from './globalOptions';
 import type {
   DocumentType,
@@ -19,13 +17,7 @@ export * from './hooks';
 export * from './plugin';
 export * from './index';
 export * from './modelOptions';
-export * from './queryMethod';
-export * from './typeguards';
-export * as defaultClasses from './defaultClasses';
-export * as errors from './internal/errors';
-// export * as types from './types';
 export { DocumentType, Ref, ReturnModelType };
-export { getClassForDocument, getClass, getName } from './internal/utils';
 export { Severity } from './internal/constants';
 
 parseENV(); // call this before anything to ensure they are applied
